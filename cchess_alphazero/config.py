996f9d8e571556fe67daecc83ae34beaa4f14a61
sid.tang <jdktomcat@163.com>
2020-06-01 16:35:39 +0800

    项目初始化-1

--- cchess_alphazero/config.py
+++ cchess_alphazero/config.py
@@ -65,7 +65,7 @@ class ResourceConfig:
         self.sl_data_move = os.path.join(self.sl_data_dir, "moves.csv")
         self.sl_onegreen = os.path.join(self.sl_data_dir, "onegreen.json")
 
-        self.font_path = os.path.join(self.project_dir, 'cchess_alphazero', 'play_games', 'PingFang.ttc')
+        self.font_path = os.path.join(self.project_dir, 'cchess_alphazero', 'play_games', '/System/Library/Fonts/PingFang.ttc')
 
     def create_directories(self):
         dirs = [self.project_dir, self.data_dir, self.model_dir, self.play_data_dir, self.log_dir,
